a5018197dbd4f1df8e7e5b0b19010e439124ade1
Saxon Payne <paynesax@oregonstate.edu>
2025-07-15 11:07:42 -0700

    🦓

--- server/main.go
+++ server/main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-
-}
-
 func main() {
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Println("Hello")
 }
