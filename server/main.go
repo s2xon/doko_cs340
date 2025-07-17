17f1cae2b5d9b7af4279be509668f24e93c3358a
Saxon Payne <paynesax@oregonstate.edu>
2025-07-17 13:11:08 -0700

    did my first query

--- server/main.go
+++ server/main.go
@@ -25,9 +25,10 @@ func main() {
 	cfg.Passwd = os.Getenv("DBPASS")
 	cfg.Net = "tcp"
 	cfg.Addr = "classmysql.engr.oregonstate.edu:3306"
-	cfg.DBName = os.Getenv("DBName")
+	cfg.DBName = os.Getenv("DBNAME")
 
 	// Get a database handle.
+	fmt.Println(cfg.FormatDSN())
 	db, err = sql.Open("mysql", cfg.FormatDSN())
 	if err != nil {
 		log.Fatal(err)
@@ -38,4 +39,19 @@ func main() {
 		log.Fatal(pingErr)
 	}
 	fmt.Println("Connected!")
+	rows, err := db.Query("SELECT * FROM Users;")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for rows.Next() {
+		var id int
+		var name string
+
+		if err := rows.Scan(&id, &name); err != nil {
+			fmt.Errorf("User %q", err)
+		}
+		fmt.Println(id, name)
+
+	}
 }
