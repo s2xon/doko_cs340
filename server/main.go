7ed5ebc0be584afe044f3e24094b6964e5bec77f
Saxon Payne <paynesax@oregonstate.edu>
2025-07-17 12:44:36 -0700

    connected db

--- server/main.go
+++ server/main.go
@@ -1,9 +1,41 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
+var db *sql.DB
+
 func main() {
-	log.Println("Hello")
+
+	var err error
+	err = godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	// Capture connection properties.
+	cfg := mysql.NewConfig()
+	cfg.User = os.Getenv("DBUSER")
+	cfg.Passwd = os.Getenv("DBPASS")
+	cfg.Net = "tcp"
+	cfg.Addr = "classmysql.engr.oregonstate.edu:3306"
+	cfg.DBName = "cs340_paynesax"
+
+	// Get a database handle.
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pingErr := db.Ping()
+	if pingErr != nil {
+		log.Fatal(pingErr)
+	}
+	fmt.Println("Connected!")
 }
